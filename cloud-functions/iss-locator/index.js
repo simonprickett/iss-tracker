9d47f91ad2ca35f8c205e8fdcafe83ac3b1fbd46
Simon Prickett <simon@crudworks.org>
2023-11-29 22:32:45 +0000

    Parses geocoder responses.

--- cloud-functions/iss-locator/index.js
+++ cloud-functions/iss-locator/index.js
@@ -5,6 +5,16 @@ const SECURITY_HEADER_NAME = 'X-ISS-Locator-Token';
 
 require('dotenv').config();
 
+function findAddressComponent(addressComponents, componentToFind) {
+  const matches = addressComponents.filter(comp => comp.types.includes(componentToFind));
+
+  if (matches.length > 0) {
+      return matches[0].long_name;
+  }
+
+  return null;
+}
+
 functions.http('isslocator', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
 
@@ -55,9 +65,6 @@ functions.http('isslocator', async (req, res) => {
   apiResponse = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?result_type=country|locality|administrative_area_level_1|natural_feature&language=en_GB&latlng=${response.lat},${response.lon}&key=${process.env.GEOCODER_API_KEY}`);
   const geocodeInfo = await apiResponse.json();
 
-  // TODO parse down the reverse geocoder response.
-  response.geo = geocodeInfo;
-
   if (geocodeInfo.status === 'ZERO_RESULTS') {
     // Try for an ocean name if possible.
     apiResponse = await fetch(`https://secure.geonames.org/oceanJSON?lat=${response.lat}&lng=${response.lon}&username=${process.env.GEONAMES_USER}`);
@@ -66,6 +73,15 @@ functions.http('isslocator', async (req, res) => {
     if (oceanInfo.ocean) {
       response.ocean = oceanInfo.ocean.name;
     }
+  } else {
+    // Retrieve fields of interest from geocoder response, if present.
+    const locality = findAddressComponent(geocodeInfo.results[0].address_components, 'locality');
+    const region = findAddressComponent(geocodeInfo.results[0].address_components, 'administrative_area_level_1');
+    const country = findAddressComponent(geocodeInfo.results[0].address_components, 'country');
+
+    if (locality) { response.locality = locality; }
+    if (region) { response.region = region; }
+    if (country) { response.country = country; }
   }
 
   return res.json(response);
